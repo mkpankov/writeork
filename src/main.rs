ebe172bcf0fa5af54a903b9abb7e3e7a2c6a7b9a
Michael Pankov <work@michaelpankov.com>
2015-06-04 01:36:58 +0300

    First working version

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,51 @@
 #![allow(non_camel_case_types)]
 
+use std::io::prelude::*;
+use std::fs::File;
+
 #[derive(Debug)]
 struct Elf64_Half (u16);
+
+#[derive(Debug)]
+struct Elf64_Word (u32);
+
+#[derive(Debug)]
+struct Elf64_Addr (u64);
+
+#[derive(Debug)]
+struct Elf64_Off (u64);
 
 const EI_NIDENT : usize = 16;
 
 #[derive(Debug)]
-struct Elf64_Ehdr<'a> {
-    e_ident: &'a [u8; EI_NIDENT],
+struct Elf64_Ehdr {
+    e_ident: [u8; EI_NIDENT],
     e_type: Elf64_Half,
+    e_machine: Elf64_Half,
+    e_version: Elf64_Word,
+    e_entry: Elf64_Addr,
+    e_phoff: Elf64_Off,
+    e_shoff: Elf64_Off,
+    e_flags: Elf64_Word,
+    e_ehsize: Elf64_Half,
+    e_phentsize: Elf64_Half,
+    e_phnum: Elf64_Half,
+    e_shentsize: Elf64_Half,
+    e_shnum: Elf64_Half,
+    e_shstrndx: Elf64_Half
+}
+
+fn work() {
+    let f = File::open(std::env::args().nth(1).unwrap()).unwrap();
+    let mut b = Vec::<u8>::with_capacity(std::mem::size_of::<Elf64_Ehdr>());
+    f.take(std::mem::size_of::<Elf64_Ehdr>() as u64).read_to_end(&mut b).unwrap();
+    let ehdr: *const Elf64_Ehdr = unsafe {
+        std::mem::transmute(b.as_ptr())
+    };
+    let e: &Elf64_Ehdr = unsafe { &*ehdr };
+    println!("{:?}", e);
 }
 
 fn main() {
-    ;
+    work();
 }
