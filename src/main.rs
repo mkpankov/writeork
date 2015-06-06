e17fb9fddf5be6683930e76d44d8897697c8f927
Michael Pankov <work@michaelpankov.com>
2015-06-06 21:45:17 +0300

    Implement OS/ABI identification

--- src/main.rs
+++ src/main.rs
@@ -85,6 +85,53 @@ impl Display for ElfEiVersion {
     }
 }
 
+#[repr(u8)]
+#[derive(Debug)]
+#[allow(dead_code)]
+enum ElfEiOsAbi {
+    ELFOSABI_NONE = 0,
+    ELFOSABI_HPUX = 1,
+    ELFOSABI_NETBSD = 2,
+    ELFOSABI_GNU = 3,
+    ELFOSABI_SOLARIS = 6,
+    ELFOSABI_AIX = 7,
+    ELFOSABI_IRIX = 8,
+    ELFOSABI_FREEBSD = 9,
+    ELFOSABI_TRU64 = 10,
+    ELFOSABI_MODESTO = 11,
+    ELFOSABI_OPENBSD = 12,
+    ELFOSABI_ARM_AEABI = 64,
+    ELFOSABI_ARM = 97,
+    ELFOSABI_STANDALONE = 255
+}
+
+#[allow(dead_code)]
+const ELFOSABI_SYSV: u8 = ElfEiOsAbi::ELFOSABI_NONE as u8;
+#[allow(dead_code)]
+const ELFOSABI_LINUX: u8 = ElfEiOsAbi::ELFOSABI_GNU as u8;
+
+impl Display for ElfEiOsAbi {
+    fn fmt(&self, fmt: &mut Formatter) -> std::fmt::Result {
+        use ElfEiOsAbi::*;
+        let s = match *self {
+            ELFOSABI_NONE => "UNIX - System V",
+            ELFOSABI_HPUX => "HP-UX",
+            ELFOSABI_NETBSD => "NetBSD",
+            ELFOSABI_GNU => "GNU ELF",
+            ELFOSABI_SOLARIS => "Sun Solaris",
+            ELFOSABI_AIX => "IBM AIX",
+            ELFOSABI_IRIX => "SGI Irix",
+            ELFOSABI_FREEBSD => "FreeBSD",
+            ELFOSABI_TRU64 => "Compaq TRU64 UNIX",
+            ELFOSABI_MODESTO => "Novell Modesto",
+            ELFOSABI_OPENBSD => "OpenBSD",
+            ELFOSABI_ARM_AEABI => "ARM EABI",
+            ELFOSABI_ARM => "ARM",
+            ELFOSABI_STANDALONE => "Standalone (embedded) application",
+        };
+        write!(fmt, "{}", s)
+    }
+}
 
 #[repr(C)]
 #[derive(Debug)]
@@ -93,7 +140,8 @@ struct ElfIdentNamed {
     ei_class: ElfEiClass,
     ei_data: ElfEiData,
     ei_version: ElfEiVersion,
-    padding2: [u8; 9],
+    ei_osabi: ElfEiOsAbi,
+    padding2: [u8; 8],
 }
 
 impl Display for ElfIdent {
@@ -146,7 +194,7 @@ impl Display for Elf64_Ehdr {
                 "  Class:                             {}\n",
                 "  Data:                              {}\n",
                 "  Version:                           {}\n",
-                "  OS ABI:                            {:?}\n",
+                "  OS/ABI:                            {}\n",
                 "  ABI Version:                       {:?}\n",
                 "  Type:                              {:?}\n",
                 "  Machine:                           {:?}\n",
@@ -166,7 +214,7 @@ impl Display for Elf64_Ehdr {
             ehdr_ident.ei_class,
             ehdr_ident.ei_data,
             ehdr_ident.ei_version,
-            "OS ABI",
+            ehdr_ident.ei_osabi,
             "ABI VERSION",
             self.e_type,
             self.e_machine,
