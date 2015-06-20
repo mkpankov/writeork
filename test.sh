d71a17d1b2ea0a8b4c7217554a4dd3d331b4c4b6
Michael Pankov <work@michaelpankov.com>
2015-06-20 21:56:21 +0300

    Fix the testing script

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/bin/zsh
-set -e
 
-diff test_results/header.pass.diff <(diff <(cargo run -- -h test_programs/be/a.out) <(readelf -h test_programs/be/a.out))
+if diff -q test_results/header.pass.diff <(diff <(cargo run -- -h test_programs/be/a.out) <(readelf -h test_programs/be/a.out)) 2>&1 > /dev/null; then
+    echo "Tests pass";
+else
+    echo "Tests fail";
+fi;
