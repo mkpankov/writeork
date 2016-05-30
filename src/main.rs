572e636e81c81e5feedd9628c1175888daf7ff3b
Michael Pankov <work@michaelpankov.com>
2016-05-30 15:24:01 +0300

    Use similar reading of Elf64_Ehdr

--- src/main.rs
+++ src/main.rs
@@ -512,6 +512,56 @@ impl Elf64_Phdr {
     }
 }
 
+fn convert_byte_vec_to_ehdr_vec(
+    v: Vec<u8>) -> Result<Vec<Elf64_Ehdr>, ()> {
+    let ehdr_size = std::mem::size_of::<Elf64_Ehdr>();
+
+    assert_eq!(ehdr_size as usize, v.len());
+    let mut r: Vec<Elf64_Ehdr> = unsafe {
+        std::mem::transmute(v)
+    };
+    unsafe {
+        r.set_len(1);
+    }
+
+    let proper_magic = &[0x7f, b'E', b'L', b'F'];
+    let magic_ptr: *const [u8; 4] = unsafe {
+        std::mem::transmute(r.as_ptr())
+    };
+    let magic = unsafe { &*magic_ptr };
+    if proper_magic != magic {
+        return Err(())
+    }
+
+    Ok(r)
+}
+
+fn convert_ehdr_vec_to_ehdr_box(
+    mut v: Vec<Elf64_Ehdr>) -> Box<Elf64_Ehdr>
+{
+    let ehdr = v.pop().unwrap();
+    let r = Box::new(ehdr);
+    r
+}
+
+fn read_ehdr<R: Read + Seek>(
+    reader: &mut R)
+    -> Box<Elf64_Ehdr>
+{
+    use std::io::SeekFrom;
+
+    let ehdr_size = std::mem::size_of::<Elf64_Ehdr>();
+    let ehdr_offset = 0;
+
+    let mut b = Vec::<u8>::with_capacity(ehdr_size as usize);
+    reader.seek(SeekFrom::Start(ehdr_offset)).unwrap();
+    reader.take(ehdr_size as u64).read_to_end(&mut b).unwrap();
+
+    convert_ehdr_vec_to_ehdr_box(
+        convert_byte_vec_to_ehdr_vec(
+            b).unwrap())
+}
+
 fn convert_byte_vec_to_phdrs_vec(
     v: Vec<u8>, phdr_num: u16, phdr_size: u16) -> Vec<Elf64_Phdr> {
     assert_eq!(phdr_num as usize * phdr_size as usize, v.len());
@@ -564,12 +614,10 @@ impl Elf64_Ehdr {
 
 fn work(options: clap::ArgMatches) {
     let path = options.value_of("FILE").unwrap();
+
     let mut f = File::open(path).unwrap();
-    let mut b = Vec::<u8>::with_capacity(std::mem::size_of::<Elf64_Ehdr>());
 
-    (&f).take(std::mem::size_of::<Elf64_Ehdr>() as u64).read_to_end(&mut b).unwrap();
-
-    let ehdr = Elf64_Ehdr::from_slice(&b).unwrap();
+    let ehdr = read_ehdr(&mut f);
 
     if options.is_present("file-header") {
         print!("{}", ehdr);
