d1a38c3e0e6bc59af984f491652ca6ce0dff3fe5
Michael Pankov <work@michaelpankov.com>
2016-06-28 22:40:55 +0300

    Implement conversion by pulling the buffer out

--- src/elf/helpers.rs
+++ src/elf/helpers.rs
@@ -1,39 +1,32 @@
 use super::b64::elf_ehdr::Elf_Ehdr as Elf64_Ehdr;
 use std::io::{Read, Seek};
 
-fn convert_byte_vec_to_ehdr_vec(
-    v: Vec<u8>) 
-    -> Result<Vec<Elf64_Ehdr>, ()> 
+fn convert_byte_vec_to_ehdr_box(
+    mut v: Vec<u8>)
+    -> Result<Box<Elf64_Ehdr>, ()>
 {
     let ehdr_size = ::std::mem::size_of::<Elf64_Ehdr>();
 
     assert_eq!(ehdr_size as usize, v.len());
-    let mut r: Vec<Elf64_Ehdr> = unsafe {
-        ::std::mem::transmute(v)
-    };
-    unsafe {
-        r.set_len(1);
-    }
+    let bytes_ptr: *mut u8 = v.as_mut_ptr();
+    ::std::mem::forget(v);
 
     let proper_magic = &[0x7f, b'E', b'L', b'F'];
     let magic_ptr: *const [u8; 4] = unsafe {
-        ::std::mem::transmute(r.as_ptr())
+        ::std::mem::transmute(bytes_ptr)
     };
     let magic = unsafe { &*magic_ptr };
     if proper_magic != magic {
         return Err(())
     }
 
-    Ok(r)
-}
-
-fn convert_ehdr_vec_to_ehdr_box(
-    mut v: Vec<Elf64_Ehdr>) 
-    -> Box<Elf64_Ehdr>
-{
-    let ehdr = v.pop().unwrap();
-    let r = Box::new(ehdr);
-    r
+    let ehdr_ptr: *mut Elf64_Ehdr = unsafe {
+        ::std::mem::transmute(bytes_ptr)
+    };
+    let ehdr_box: Box<Elf64_Ehdr> = unsafe {
+        Box::from_raw(ehdr_ptr)
+    };
+    Ok(ehdr_box)
 }
 
 pub fn read_ehdr<R: Read + Seek>(
@@ -49,7 +42,5 @@ pub fn read_ehdr<R: Read + Seek>(
     reader.seek(SeekFrom::Start(ehdr_offset)).unwrap();
     reader.take(ehdr_size as u64).read_to_end(&mut b).unwrap();
 
-    convert_ehdr_vec_to_ehdr_box(
-        convert_byte_vec_to_ehdr_vec(
-            b).unwrap())
+    convert_byte_vec_to_ehdr_box(b).unwrap()
 }
