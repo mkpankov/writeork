8482e885d01511312c00e7bae9c0258447a1ad09
Michael Pankov <work@michaelpankov.com>
2016-07-16 18:04:37 +0300

    Move Ehdr reading from helpers to core

--- src/elf/b32/elf_ehdr.rs
+++ src/elf/b32/elf_ehdr.rs
@@ -5,5 +5,3 @@ use super::primitive::*;
 elf_ehdr!();
 
 pub type Elf32_Ehdr = Elf_Ehdr<Elf32_Half, Elf32_Word, Elf32_Addr, Elf32_Off>;
-
-read_ehdr!();
--- src/elf/b64/elf_ehdr.rs
+++ src/elf/b64/elf_ehdr.rs
@@ -4,5 +4,3 @@ use super::primitive::*;
 elf_ehdr!();
 
 pub type Elf64_Ehdr = Elf_Ehdr<Elf64_Half, Elf64_Word, Elf64_Addr, Elf64_Off>;
-
-read_ehdr!();
